97bff4f6f23ced4971b62b83585cbf373e6b8065
Quan <olivernham6@gmail.com>
2023-08-15 23:10:08 -0400

    quan

--- config.py
+++ config.py
@@ -4,8 +4,9 @@ sample_folders = [
     '~/Desktop/stuff/music/Sophie Samples',
     '~/Desktop/stuff/tidal/spicule/extra-samples',
     '~/Library/Application Support/SuperCollider/downloaded-quarks/Dirt-Samples',
-    '~/Desktop/stuff/music/stems/arca/textures'
+    '~/Desktop/stuff/tsx-import'
 ]
 
 segment_csv = '~/Desktop/stuff/tidal/segments220917.csv'
+# segment_csv = '~/Desktop/stuff/tidal/segments_new.csv'
 
